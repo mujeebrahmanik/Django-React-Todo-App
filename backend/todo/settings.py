a9ecaf32e0859e7891c302adcd963fa9d25622c6
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2025-11-24 23:05:16 +0400

    deployment changes in backend

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-
 ]
 
 MIDDLEWARE = [
@@ -125,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- backend/todo/wsgi.py
+++ backend/todo/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+settings_module = 'todo.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' else 'todo.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
