ff34337f69b131f802a6876947c710586b83288c
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2025-11-25 15:38:07 +0400

    error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Mytodos from './components/Mytodos'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,useLocation} from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import AuthProvider from './AuthProvider'
 import PrivateRoute from './PrivateRoute'
 import Category from './components/Category'
+
+
+function AppRoutes() {
+  const location = useLocation();
+  const hideHeader = location.pathname === '/'; 
+
+  return (
+    <>
+      {!hideHeader && <Header />}
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/todo' element={<PrivateRoute><Mytodos /></PrivateRoute>} />
+        <Route path='/category' element={<PrivateRoute><Category /></PrivateRoute>} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,13 +34,7 @@ function App() {
     <>
     <AuthProvider>
     <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route path='/todo' element={<PrivateRoute><Mytodos/></PrivateRoute>}/>
-          <Route path='/category' element={<PrivateRoute><Category/></PrivateRoute>}/>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
+      <AppRoutes/>
     </BrowserRouter>
     </AuthProvider>
       
